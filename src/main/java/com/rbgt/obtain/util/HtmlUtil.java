9bc7ca03357df44792bf72e671a2d6174d9166e0
俞春旺 <1228368500@qq.com>
2020-06-21 19:29:52 +0800

    篮球赛事 - 修改工具类

--- src/main/java/com/rbgt/obtain/util/HtmlUtil.java
+++ src/main/java/com/rbgt/obtain/util/HtmlUtil.java
@@ -25,32 +25,36 @@ public class HtmlUtil {
 	private NbaTquizInfoCopyMapper nbaTquizInfoCopyMapper;
 	
 	public static void main(String[] args) {
-		
-		
-		
-		final WebClient webClient = new WebClient(BrowserVersion.CHROME);//新建一个模拟谷歌Chrome浏览器的浏览器客户端对象
-
-        webClient.getOptions().setThrowExceptionOnScriptError(false);//当JS执行出错的时候是否抛出异常, 这里选择不需要
-        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);//当HTTP的状态非200时是否抛出异常, 这里选择不需要
+		//新建一个模拟谷歌Chrome浏览器的浏览器客户端对象
+		final WebClient webClient = new WebClient(BrowserVersion.CHROME);
+		//当JS执行出错的时候是否抛出异常, 这里选择不需要
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+		//当HTTP的状态非200时是否抛出异常, 这里选择不需要
+        webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
         webClient.getOptions().setActiveXNative(false);
-        webClient.getOptions().setCssEnabled(false);//是否启用CSS, 因为不需要展现页面, 所以不需要启用
-        webClient.getOptions().setJavaScriptEnabled(true); //很重要，启用JS
-        webClient.setAjaxController(new NicelyResynchronizingAjaxController());//很重要，设置支持AJAX
+		//是否启用CSS, 因为不需要展现页面, 所以不需要启用
+        webClient.getOptions().setCssEnabled(false);
+		//很重要，启用JS
+        webClient.getOptions().setJavaScriptEnabled(true);
+		//很重要，设置支持AJAX
+        webClient.setAjaxController(new NicelyResynchronizingAjaxController());
 
         HtmlPage page = null;
         try {
-            //page = webClient.getPage("https://cba.hupu.com/gamespace/?lun=14");//尝试加载上面图片例子给出的网页
-            page = webClient.getPage("http://score.nowscore.com/basketball.htm?date=2020-1-4");//尝试加载上面图片例子给出的网页
+			//尝试加载上面图片例子给出的网页
+            page = webClient.getPage("http://score.nowscore.com/basketball.htm?date=2020-06-20");
             
         } catch (Exception e) {
             e.printStackTrace();
         }finally {
             webClient.close();
         }
-        webClient.waitForBackgroundJavaScript(30000);//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
-        String pageXml = page.asXml();//直接将加载完成的页面转换成xml格式的字符串
-
-        Document document = Jsoup.parse(pageXml);//获取html文档
+		//异步JS执行需要耗时,所以这里线程要阻塞30秒,等待异步JS执行结束
+        webClient.waitForBackgroundJavaScript(30000);
+		//直接将加载完成的页面转换成xml格式的字符串
+        String pageXml = page.asXml();
+		//获取html文档
+        Document document = Jsoup.parse(pageXml);
         
         
         int len = document.getElementById("live").getElementsByTag("table")
@@ -58,7 +62,7 @@ public class HtmlUtil {
 
 		Calendar now = Calendar.getInstance();
 		// 获取当日天数+1,获取明天比赛信息
-		String day = (now.get(Calendar.DAY_OF_MONTH) + 1) + "";
+		String day = (now.get(Calendar.DAY_OF_MONTH)) + "";
 		int day_len = day.length();
 		if(1 == day_len)
 		{
